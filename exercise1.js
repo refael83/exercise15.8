2c305b1cdf4b7394b29d5410c416992126c706a1
רפאל אדרי <you@example.com>
2023-08-16 16:00:37 +0300

    correction

--- exercise1.js
+++ exercise1.js
@@ -1,113 +1,118 @@
-const data=[{
+const users = [
+  {
     id: "1",
-    email:'gadi@gmail',
-    password:'123456'
-},{
+    email: "gadi@gmail",
+    password: "123456",
+  },
+  {
     id: "2",
-    email:'dana@gmail',
-    password:'234567'
-},{
-    id: "3", 
-    email:'salome@gmail',
-    password:'345678'
-}]
-function checkID(newID){
-    data.forEach((element)=>{
-        if(element.id===newID)return 0
-    })
-    return 1
+    email: "dana@gmail",
+    password: "234567",
+  },
+  {
+    id: "3",
+    email: "salome@gmail",
+    password: "345678",
+  },
+];
+function checkEmail(newEmail) {
+  users.forEach((user) => {
+    if (user.email === newEmail) return 0;
+  });
+  return 1;
 }
 
-const express=require('express')
-const bcrypt=require('bcrypt')
+function validateEmail(email) {
+    let regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
+  }
+
+  function checkPassword(password){
+    let regex=/^(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+  }
+ //function checkInput(){}
+  
+
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
-const port= 3000
-app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send(data)
-})
-app.get('/users/:id',(req,res)=>{
-    let j=0
-    for(let i=0;i<data.length;i++){
-        if(data[i].id==req.params.id){
-           res.send(data[i])
-           j=1
-        }
+const port = 3000;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(users);
+});
+//stage 1
+app.get("/users/:id", (req, res) => {
+   let flag = users.find((user)=>user.id===req.params.id);
+   console.log(flag)
+    if(flag)
+     res.send(flag);
+    else  
+     res.send("not exist");
+});
+
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+  let newPassword = newUser.password;
+  bcrypt.hash(newPassword, 10, function (err, hash) {
+    newUser.password = hash;
+    newUser.id = uuidv4();
+    if (checkEmail(newUser.email)&&validateEmail(newUser.email)&&checkPassword) {
+      users.push(newUser);
+      res.send(users);
+    } else {
+      res.send("inter correct input");
     }
-    if(j==0){
-        res.send('not exist')
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  let j=users.findIndex((user)=>user.id===req.params.id);
+  if(j>=0){
+    users[j] = req.body;
+    res.send(users)
+  }else{
+    res.send("not exist");
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+    let j=users.findIndex((user)=>user.id == req.params.id);
+    console.log(j)
+    if(j>=0) users.splice(j, 1);
+    j>=0?res.send(users):res.send("not exist");
+  
+});
+
+app.post("/exist", (req, res) => {
+  let exist =users.findIndex((user)=>user.email===req.body.email);
+  if(exist>=0){
+    let password=bcrypt.compareSync(element.password, req.body.password);
+    if (password || req.body.password === element.password){
+        res.send("User is connected");
     }
-    
-})
-
-app.post('/users',(req,res)=>{
-    let newUser=req.body;
-    const {v4: uuidv4}=require('uuid')
-    let newPassword=newUser.password
-    bcrypt.hash(newPassword,10,function(err,hash){
-       newUser.password=hash
-       newUser.id=uuidv4()
-       if(checkID(newUser.id)){
-        data.push(newUser)
-        res.send(data);
-       }
-       else{
-        res.send("id is already exist")
-       }
-       
-    })
-    
-})
-
-app.put('/users/:id',(req,res)=>{
-    for(let i=0;i<data.length;i++){
-        if(data[i].id==req.params.id){
-           data[i]=req.body
-        }
+    else{
+        res.send(" wrong credentials");
     }
-    res.send(data)
-})
+  }
 
-app.delete('/users/:id',(req,res)=>{
-    for(let i=0;i<data.length;i++){
-        if(data[i].id==req.params.id){
-           data.splice(i,1)
-        }
-    }
-    res.send(data)
-})
+//   users.forEach((element) => {
+//     if (element.email === req.body.email) {
+//       let password = bcrypt.compareSync(element.password, req.body.password);
+//       if (password || req.body.password === element.password) {
+//         exist = 1;
+//         res.send("User is connected");
+//       }
+//     }
+//     if (i == users.length - 1 && exist == 0) {
+//       res.send(" wrong credentials");
+//     }
 
-app.post('/exist',(req,res)=>{
-    let exist=0   
-    let i=0 
-    data.forEach((element)=>{
-        console.log(element.password)
-       if(element.email===req.body.email){
-        let password=bcrypt.compareSync(element.password,req.body.password)
-        if(password||req.body.password===element.password){
-            exist=1
-            res.send("User is connected");
-        }
-        // bcrypt.compare(req.body.password,element.password).then(function(err,result){
-        //     console.log(element.password)
-        //     if(result==true||element.password===req.body.password){
-        //         exist=1
-        //         console.log(element.password)
-        //         res.send("User is connected");
-        //     }
-        // })
-      }
-       if(i==data.length-1&&exist==0){
-        res.send(' wrong credentials')
-      }
-       
-      i++;
-    })
-    
-})
+//     i++;
+//   });
+});
 
-
-app.listen(port,()=>{
-    console.log(`Server is up and running on port:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Server is up and running on port:${port}`);
+});
