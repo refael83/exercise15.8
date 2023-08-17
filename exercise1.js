d17c7ee12dd811a276a14a4933d22e7f393bcabc
רפאל אדרי <you@example.com>
2023-08-17 15:20:53 +0300

    stage 6

--- exercise1.js
+++ exercise1.js
@@ -1,4 +1,4 @@
-const users = [
+const data = [
   {
     id: "1",
     email: "gadi@gmail",
@@ -19,9 +19,18 @@ const users = [
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
+const jsonfile = require("jsonfile");
 const app = express();
 const port = 3000;
 app.use(express.json());
+const file = "data.json";
+jsonfile.writeFile(file, data, function (err) {
+  if (err) console.log(err);
+});
+jsonfile.readFile(file, function (err, obj) {
+  if (err) console.error(err);
+  console.dir(obj);
+});
 
 function checkEmail(newEmail) {
   users.forEach((user) => {
@@ -31,37 +40,50 @@ function checkEmail(newEmail) {
 }
 
 function validateEmail(email) {
-    let regex = /\S+@\S+\.\S+/;
-    return regex.test(email);
-  }
+  let regex = /\S+@\S+\.\S+/;
+  return regex.test(email);
+}
 
-  function checkPassword(password){
-    let passw=/^(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-    return passw.test(password)
-  }
- //function checkInput(){}
+function checkPassword(password) {
+  let passw = /^(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+  return passw.test(password);
+}
+//function checkInput(){}
 
 app.get("/", (req, res) => {
+  jsonfile.readFile(file, (err, users) => {
   res.send(users);
+  })
 });
 //stage 1
 app.get("/users/:id", (req, res) => {
-   let flag = users.find((user)=>user.id===req.params.id);
-   console.log(flag)
-    if(flag)
-     res.send(flag);
-    else  
-     res.send("not exist");
+  jsonfile.readFile(file, (err, users) => {
+    let flag = users.find((user) => user.id === req.params.id);
+    console.log(flag);
+    if (flag) res.send(flag);
+    else res.send("not exist");
+  });
 });
 
 app.post("/users", (req, res) => {
+  let users = jsonfile.readFile(file, function (err, obj) {
+    if (err) console.error(err);
+    console.dir(obj);
+  });
   let newUser = req.body;
   let newPassword = newUser.password;
   bcrypt.hash(newPassword, 10, function (err, hash) {
     newUser.password = hash;
     newUser.id = uuidv4();
-    if (checkEmail(newUser.email)&&validateEmail(newUser.email) &&checkPassword(newPassword)){
+    if (
+      checkEmail(newUser.email) &&
+      validateEmail(newUser.email) &&
+      checkPassword(newPassword)
+    ) {
       users.push(newUser);
+      jsonfile.writeFile(file,users, function(arr){
+         if (err) console.log(err);
+      });
       res.send(users);
     } else {
       res.send("inter correct input");
@@ -70,31 +92,36 @@ app.post("/users", (req, res) => {
 });
 
 app.put("/users/:id", (req, res) => {
-  let j=users.findIndex((user)=>user.id===req.params.id);
-  if(j>=0){
+  let users = jsonfile.readFile(file, function (err, obj) {
+    if (err) console.error(err);
+  });
+  let j = users.findIndex((user) => user.id === req.params.id);
+  if (j >= 0) {
     users[j] = req.body;
-    res.send(users)
-  }else{
+    jsonfile.writeFile(file,users, function(arr){
+      if (err) console.log(err);
+   });
+    res.send(users);
+  } else {
     res.send("not exist");
   }
 });
 
 app.delete("/users/:id", (req, res) => {
-    let j=users.findIndex((user)=>user.id == req.params.id);
-    if(j>=0) users.splice(j, 1);
-    j>=0?res.send(users):res.send("not exist");
+  let j = users.findIndex((user) => user.id === req.params.id);
+  if (j >= 0) users.splice(j, 1);
+  j >= 0 ? res.send(users) : res.send("not exist");
 });
 
 app.post("/exist", (req, res) => {
-  let exist =users.findIndex((user)=>user.email===req.body.email);
-  if(exist>=0){
-    let password=bcrypt.compareSync(users[exist].password, req.body.password);
+  let exist = users.findIndex((user) => user.email === req.body.email);
+  if (exist >= 0) {
+    let password = bcrypt.compareSync(users[exist].password, req.body.password);
     if (password || req.body.password === users[exist].password)
-        res.send("User is connected")
+      res.send("User is connected");
+  } else {
+    res.send(" wrong credentials");
   }
-    else{
-        res.send(" wrong credentials");
-    }
 });
 
 app.listen(port, () => {
